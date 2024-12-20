b4a222923801422063c62b86edd9a33206e4502c
Danil <fofogogo12345689@mail.ru>
2024-12-20 13:40:12 +0500

    fixed file_processing

--- src/file_processing.py
+++ src/file_processing.py
@@ -1,8 +1,8 @@
 import pandas as pd
-from typing import List, Dict
+from typing import List, Dict, Any
 
 
-def read_transactions_from_csv(file_path: str) -> List[Dict]:
+def read_transactions_from_csv(file_path: str) -> List[Dict[str, Any]]:
     """
     Считывает финансовые операции из CSV-файла и возвращает список словарей.
 
@@ -10,8 +10,8 @@ def read_transactions_from_csv(file_path: str) -> List[Dict]:
     :return: Список транзакций в виде словарей.
     """
     try:
-        data = pd.read_csv(file_path)
-        transactions = data.to_dict(orient="records")
+        data: pd.DataFrame = pd.read_csv(file_path)
+        transactions: List[Dict[str, Any]] = data.to_dict(orient="records")
         return transactions
     except FileNotFoundError:
         raise FileNotFoundError(f"CSV file not found: {file_path}")
@@ -19,7 +19,7 @@ def read_transactions_from_csv(file_path: str) -> List[Dict]:
         raise ValueError(f"CSV file is empty or invalid: {file_path}")
 
 
-def read_transactions_from_excel(file_path: str) -> List[Dict]:
+def read_transactions_from_excel(file_path: str) -> List[Dict[str, Any]]:
     """
     Считывает финансовые операции из Excel-файла и возвращает список словарей.
 
@@ -27,8 +27,8 @@ def read_transactions_from_excel(file_path: str) -> List[Dict]:
     :return: Список транзакций в виде словарей.
     """
     try:
-        data = pd.read_excel(file_path)
-        transactions = data.to_dict(orient="records")
+        data: pd.DataFrame = pd.read_excel(file_path)
+        transactions: List[Dict[str, Any]] = data.to_dict(orient="records")
         return transactions
     except FileNotFoundError:
         raise FileNotFoundError(f"Excel file not found: {file_path}")
